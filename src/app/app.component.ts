da8b8664540c3d2f401b25934b79d5c6453eae17
Victor Baggio <baggiobaggiovictor@gmail.com>
2022-06-15 19:28:37 -0300

    criando o component task

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,4 +1,5 @@
 import { Component } from '@angular/core';
+import { Task } from './task/task'
 
 @Component({
   selector: 'app-root',
@@ -7,4 +8,14 @@ import { Component } from '@angular/core';
 })
 export class AppComponent {
   title = 'kanban-fire';
+  todo: Task[] = [
+    {
+      title: 'Comprar leite',
+      description: 'Ir ao mercado comprar leite'
+    },
+    {
+      title: 'Desenvolver o kanban',
+      description: 'Usar firebase como banco de dados do kanban '
+    }
+  ]
 }
